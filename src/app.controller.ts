7b45f341c9da70e8030dba964394c727dd0ec6c0
NahoKizawa <n.kizawa.629@gmail.com>
2021-12-04 11:29:34 +0900

    Ajax通信

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  // service.tsで宣言したクラスをDI
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index') // index.ejsテンプレートを使うことを指定
+  root() {
+    // index.ejsテンプレートに必要な情報をまとめてreturn
+    return {
+      title: 'Nest sample app',
+      msg: 'messagemessage'
+    }
+  }
+
+  @Post('/')
+  send(@Body() form:any) {
+    return form
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { resolve, join } from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,)
+
+  // 静的ファイルの保管場所を指定「public」フォルダにイメージファイルなどのリソースを配置する
+  app.useStaticAssets(join(__dirname, '..', 'public'))
+  // ビューテンプレートの保管場所を設定「views」フォルダにテンプレートファイルを配置する
+  app.setBaseViewsDir(resolve('./src/views'))
+  // テンプレートエンジンをejsに設定
+  app.setViewEngine('ejs')
+
+  // 3000番ポートでサーバの起動
   await app.listen(3000);
 }
 bootstrap();
